509027a1eda68b43e78f582945852ca42597af02
KristinAlex1 <kristinalex2001@gmail.com>
2024-11-03 20:40:37 -0500

    bubblesort added

--- app.js
+++ app.js
@@ -1,51 +1,69 @@
 
 let cont = document.querySelector(".container");
 let arr = [];
+let start = document.querySelector("#create-bars");
 let bubble = document.querySelector("#bubble");
 
+start.addEventListener("click",createBars);
+bubble.addEventListener("click", () => {
+    let val = createBars();
+    bubbleSort(val);
 
 
-function createBars(){
-
-    for (let i = 0;i<30;i++){
-        arr[i] = Math.random();
+})
     
-    }
 
-    for (let i = 0;i < arr.length;i++){
+
+
+
+function createBars() {
+
+    cont.innerHTML = "";
+    arr = [];
+    for(let i = 0;i<30;i++){
+        arr[i] = Math.random(); 
+    }
+    for(let i = 0;i<arr.length;i++){
         let bar = document.createElement("div");
-        bar.style.backgroundColor = "red";
-        bar.style.height = arr[i]*100 + "px";
+        bar.style.height = arr[i] * 100 + "px";
         bar.classList.add("bar");
         cont.appendChild(bar);
     }
+    return arr;
 }
 
+function bubbleSort(arr){
+    let i = 0;
+    let swapped; 
+    do{
+        swapped = false;
+        for(i = 0;i < arr.length;i++){
+            if(arr[i]<arr[i-1]){
+                let temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = temp;
+                swapped = true;
+                updateBars();
 
-function bubbleSort(arr) {
-    let len = arr.length;
-    let swapped;
-
-    // Loop through the entire array
-    for (let i = 0; i < len; i++) {
-        swapped = false; // Track if any elements are swapped in this pass
-
-        // Compare adjacent elements and swap if needed
-        for (let j = 0; j < len - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                // Swap elements
-                let temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-
-                swapped = true; // Mark that a swap has occurred
-            }
-        }
-
-        // If no elements were swapped, the array is already sorted
-        if (!swapped) {
-            break;
         }
     }
-    return array; // Return the sorted array
+    }while(swapped)
+
+    ;
+
 }
+
+function updateBars(){
+    cont.innerHTML = "";
+    for(let i = 0;i<arr.length;i++){
+        let bar = document.createElement("div");
+        bar.style.height = arr[i] * 100 + "px";
+        bar.classList.add("bar");
+        cont.appendChild(bar);
+    }
+    return arr;
+
+}
+
+
+
